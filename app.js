743a920cc42a17e4681d44bc9c8988f7639fbcb5
Benjamin Sabardu <bsabardu@gmail.com>
2020-04-20 09:52:27 +0200

    End CRUD chapter starting security

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const ini = require('ini');
 const fs = require('fs');
-const Thing = require('./models/thing.js');
+const stuffRoutes = require('./routes/stuff')
 
 const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
 
@@ -29,29 +29,9 @@ app.use((req,res,next) =>{
     next();
 });
 
-app.post('/api/stuff', (req,res,next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-    .then(() => res.status(201).json({message: "L'objet est bien sauvegardé dans la base"}))
-    .catch(error => res.status(400).json({error}));
-});
-
-
-app.get('/api/stuff/:id', (req,res,next) => {
-    Thing.findOne({_id: req.params.id})
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json(error));
-})
-
-app.use('/api/stuff', (req,res,next) => {
-    Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({error}));
-})
+app.use('/api/suff', stuffRoutes);
 
 
 
 module.exports = app; 
+
