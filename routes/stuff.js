7c8b7eb65bf6ff4776d3e3a5f66a8493fc7663d8
Benjamin Sabardu <bsabardu@MacBook-Pro-de-Benjamin.local>
2020-07-28 18:28:28 +0200

    End auth + Start multer config

--- routes/stuff.js
+++ routes/stuff.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Thing = require('../models/Thing');
 const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth')
 
-router.post('/', stuffCtrl.createThing);
-router.get('/', stuffCtrl.readAllThing);
-router.get('/:id', stuffCtrl.readOneThing);
-router.put('/:id', stuffCtrl.updateThing);
-router.delete('/:id', stuffCtrl.deleteThing);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/', auth, stuffCtrl.readAllThing);
+router.get('/:id', auth, stuffCtrl.readOneThing);
+router.put('/:id', auth, stuffCtrl.updateThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
